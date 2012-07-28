feb6f5adfc0e0c6f6eada92b6271752f85d8c624
Tom Hudson <mail@tomnomnom.com>
2012-07-28 19:57:03 +0100

    TomNomNom slide progress

--- sheep.php
+++ sheep.php
@@ -7,32 +7,46 @@ $alpha = include __DIR__.'/letters.php';
 $screen = new Screen(101, 31);
 $screen->clear();
 
-$sheepySprite = array_map(function($line){
-  return trim($line, "\n");
-}, file(__DIR__."/images/tomnomnom.jpg.txt"));
+$heading = new Actor([23,-6], function($screen) use($alpha){
+  $this->slideY(1,1);
+  $screen->spriteWord($this->c, 'TomNomNom', $alpha);
+});
+$screen->attachActor($heading);
 
-$bio = new Actor([60,5], function($screen){
+
+$bio = new Actor([100,7], function($screen){
   static $targetText = null;
   static $displayText;
 
   if (is_null($targetText)){
-    $targetText = str_split("This is the bio text and it \nis too long to fit on the screen");
+    $targetText = str_split(
+      "- DevOps Engineer\n".
+      "- PHP Hacker\n".
+      "- Not really a sheep\n"
+    );
   }
+
+  $this->slideX(60, 2);
 
   $displayText .= array_shift($targetText);
-  $screen->text($this->c, $displayText);
+  $screen->text($this->c, $displayText, 25);
 });
 
-$sheepyActor = new Actor([100,30], function($screen) use($sheepySprite, $bio){
-  $this->slideY(2, 1);
-  $xFinished = $this->slideX(2, 3);
-  $screen->sprite($this->c, $sheepySprite);
 
-  if ($xFinished){
+$sheepySprite = array_map(function($line){
+  return trim($line, "\n");
+}, file(__DIR__."/images/tomnomnom.jpg.txt"));
+
+$sheepy = new Actor([1,30], function($screen) use($sheepySprite, $bio){
+
+  if ($this->slideY(6, 2)){
     $screen->attachActor($bio); 
   }
+
+  $screen->sprite($this->c, $sheepySprite);
 });
-$screen->attachActor($sheepyActor);
+$screen->attachActor($sheepy);
+
 
 $border = new Actor([0,0], function($screen){
   $screen->rect($this->c, [100,30], '#');
