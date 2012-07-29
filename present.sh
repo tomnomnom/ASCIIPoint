4c9b16f55ae47fcd82139c75e28e1bec80e11888
Gareth Halfacree <gareth@halfacree.co.uk>
2012-07-29 10:52:48 +0100

    Made present.sh enter-to-advance.

--- present.sh
+++ present.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 for i in `ls example-slides/*.php`; do
   php $i&
-  sleep 10
+  #sleep 10
+  read -p ""
   kill `ps axufw | grep php | grep example-slides | grep -v grep | awk '{print $2}'`
 done
