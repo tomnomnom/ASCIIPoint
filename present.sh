16a8a724233b5acd2c9e954c1bd513bdcac89672
Gareth Halfacree <gareth@halfacree.co.uk>
2012-07-29 10:58:42 +0100

    Suppress error messages on killing PHP.

--- present.sh
+++ present.sh
@@ -3,6 +3,7 @@ for i in `ls example-slides/*.php`; do
   php $i&
   #sleep 10
   read -p ""
-  kill `ps axufw | grep php | grep example-slides | grep -v grep | awk '{print $2}'`
+  kill `ps axufw | grep php | grep example-slides | grep -v grep | awk '{print $2}'` &> /dev/null
 done
+clear
 clear
