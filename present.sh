a9721f68f642f78b73bcb373dcbca5a7f1c5d5cd
Gareth Halfacree <gareth@halfacree.co.uk>
2012-07-29 10:36:31 +0100

    Removed -9 flag from kill in present.sh

--- present.sh
+++ present.sh
@@ -2,5 +2,5 @@
 for i in `ls example-slides/*.php`; do
   php $i&
   sleep 10
-  kill -9 `ps axufw | grep php | grep example-slides | grep -v grep | awk '{print $2}'`
+  kill `ps axufw | grep php | grep example-slides | grep -v grep | awk '{print $2}'`
 done
