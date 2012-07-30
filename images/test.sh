2a7e474cd0c339dc41945cefd52baac24bac0cdd
Gareth Halfacree <gareth@halfacree.co.uk>
2012-07-30 20:08:41 +0100

    Added slides directory, modified present.sh accordingly

---
+++ images/test.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+for i in `seq 100`; do
+jp2a --height=26 -i php.jpg > /dev/null
+done
--- present.sh
+++ present.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-for i in `ls example-slides/*.php`; do
+for i in `ls example-slides/*.php`; do # Change to slides/*.php if you're using your own rather than the examples.
   php $i&
   #sleep 10
   read -p ""
-  kill `ps axufw | grep php | grep example-slides | grep -v grep | awk '{print $2}'` &> /dev/null
+  kill `ps axufw | grep php | grep slides | grep -v grep | awk '{print $2}'` &> /dev/null
 done
 clear
 clear
